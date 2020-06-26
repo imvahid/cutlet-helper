6add55c33701abe0fe86143d328eafd134aba873
Vahid Ashourzadeh <v.ashourzadeh@gmail.com>
2020-06-26 13:53:12 +0430

    The Validation class mode was deleted for validations repository and the extend mode was added

--- src/CutletHelper/CutletHelperServiceProvider.php
+++ src/CutletHelper/CutletHelperServiceProvider.php
@@ -5,7 +5,6 @@ namespace Va\CutletHelper;
 use Illuminate\Support\ServiceProvider;
 use Va\CutletHelper\Facades\CutletHelper;
 use Va\CutletHelper\Helpers\Helper;
-use Va\CutletHelper\Validations\HelperValidation;
 
 class CutletHelperServiceProvider extends ServiceProvider
 {
@@ -26,28 +25,10 @@ class CutletHelperServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app['validator']->resolver(function($translator, $data, $rules) {
-            return new HelperValidation($translator, $data, $rules, $this->messages());
-        });
+        require_once(__DIR__ . '/Validations/helperValidation.php');
 
         $this->publishes([
             __DIR__ . '/../config/cutlet-helper.php' => config_path('cutlet-helper.php'),
         ], 'cutlet-helper');
-    }
-
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function messages()
-    {
-        return [
-            'national_code' => config('cutlet-helper.national_code'),
-            'iban' => config('cutlet-helper.iban'),
-            'debit_card' => config('cutlet-helper.debit_card'),
-            'postal_code' => config('cutlet-helper.postal_code'),
-            'shenase_meli' => config('cutlet-helper.shenase_meli'),
-        ];
     }
 }
