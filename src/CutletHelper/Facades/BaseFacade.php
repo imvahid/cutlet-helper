f14224145cf4e9eadf107ece601e0d1cbab8498b
Vahid Ashourzadeh <v.ashourzadeh@gmail.com>
2020-10-19 08:51:26 +0330

    Dependency of service to another packeage was cuted

--- src/CutletHelper/Facades/BaseFacade.php
+++ src/CutletHelper/Facades/BaseFacade.php
@@ -2,9 +2,17 @@
 
 namespace Va\CutletHelper\Facades;
 
-use Imanghafoori\SmartFacades\Facade as CustomFacade;
+use Illuminate\Support\Facades\Facade;
 
-abstract class BaseFacade extends CustomFacade
+class BaseFacade extends Facade
 {
-    //
+    protected static function getFacadeAccessor()
+    {
+        return static::class;
+    }
+
+    public static function shouldProxyTo($class)
+    {
+        app()->bind(self::getFacadeAccessor(), $class);
+    }
 }
